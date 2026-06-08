55d56c7d7fdcda51d758d8ca24381e2e3629a0e7
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-06-08 09:41:51 +0530

    remove duplicate config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups"
+          }
+        ]
+      }
+    ]
+  },
   images: {
     // dangerouslyAllowLocalIP: true,
     remotePatterns: [
